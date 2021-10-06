d6cb0b6c03727720f7a8815a6a68bff21a8c0a23
mohdhanif1234 <hanif.topia@gmail.com>
2021-10-06 07:25:30 +0530

    [Hanif]:Added a program to store and the print the basic details of a person in the form of a list

---
+++ PersonDataManagement/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace PersonDataManagement
+{
+    internal class Person
+    {
+        // Declaring different fields for a person
+        public int SSN { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ PersonDataManagement/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonDataManagement
+{
+    class Program
+    {
+        // UC-1: Creating a person list which contains basic details like SSN, name, address and age
+        static void Main(string[] args)
+        {
+            List<Person> list = new List<Person>();
+            AddPersonDetails(list); // Calling method to add person details
+            Console.ReadLine();
+        }
+        
+        public static void AddPersonDetails(List<Person> list)
+        {
+            list.Add(new Person() { SSN = 1, Name = "Shrikanth", Address = "Bangalore", Age = 14 });
+            list.Add(new Person() { SSN = 2, Name = "UV", Address = "Mumbai", Age = 70 });
+            list.Add(new Person() { SSN = 3, Name = "Arun", Address = "Chennai", Age = 32 });
+            list.Add(new Person() { SSN = 4, Name = "Kalpana", Address = "Delhi", Age = 16 });
+            list.Add(new Person() { SSN = 5, Name = "Sachin", Address = "Pune", Age = 63 });
+            list.Add(new Person() { SSN = 6, Name = "Arun", Address = "Kolkata", Age = 36 });
+            IterateOverLoop(list);
+        }
+
+        // Displaying the person details from the list
+        public static void IterateOverLoop(List<Person> list)
+        {
+            foreach (Person person in list)
+            {
+                Console.WriteLine("SSN={0}\tName={1}\tAddress={2}\tAge={3}", person.SSN, person.Name, person.Address, person.Age);
+            }
+        }
+    }
+}
